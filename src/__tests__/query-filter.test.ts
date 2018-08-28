1edac0af3b5d2c863e924f0917800f11fdf3ca0e
Andre Cardoso Montagnini <andre.montagnini@paytechholding.com>
2018-08-28 15:40:51 -0300

    Tests Unitarios

--- src/__tests__/query-filter.test.ts
+++ src/__tests__/query-filter.test.ts
@@ -3,9 +3,15 @@ import { Order, Operator } from '../enums';
 import { Filter } from '../filter';
 import { GenericFilter } from '../generic-filter';
 
-test('Clean Filters', () => {
+test('Operador "or" en la busqueda generica.', () => {
   let queryFilter = new QueryFilter('name', Order.Asc);
   queryFilter.generic = new GenericFilter(["name"], "alfredo");
   queryFilter.filter = [new Filter("age", "23")];
   expect(queryFilter.compile().operator).toBe(Operator.Or);
+});
+
+test('Operador "and" en la busqueda no generica.', () => {
+  let queryFilter = new QueryFilter('name', Order.Asc);
+  queryFilter.filter = [new Filter("age", "23")];
+  expect(queryFilter.compile().operator).toBe(Operator.And);
 });
